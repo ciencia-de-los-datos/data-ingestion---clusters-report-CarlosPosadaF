9ecf98d495aff7fd1663340bdf3caf4c37686c66
CarlosPosadaF <cposadaf@unal.edu.co>
2023-03-21 16:48:11 -0500

    Entrega deinitiva 2

--- pregunta.py
+++ pregunta.py
@@ -17,22 +17,22 @@ def ingest_data():
     df = pd.read_fwf("clusters_report.txt", colspecs=[(3,5),(9,14),(25,29),(40,119)], header=None)
     df.drop(df.index[:3], inplace=True)
     df.reset_index(drop=True, inplace=True)
-    df2 = pd.DataFrame()
-    df2['cluster'] = df[0]
-    df2['cantidad_de_palabras_clave'] = df[1]
-    df2['porcentaje_de_palabras_clave'] = df[2].str.replace(',', '.')
-    df2.dropna(inplace=True)
-    df2.reset_index(drop=True,inplace=True)
-    df2['cluster'] = df2['cluster'].str.strip().astype(int)
-    df2['cantidad_de_palabras_clave'] = df2['cantidad_de_palabras_clave'].str.strip().astype(int)
-    df2['porcentaje_de_palabras_clave'] = df2['porcentaje_de_palabras_clave'].str.strip().astype(float)
+    dfSalida = pd.DataFrame()
+    dfSalida['cluster'] = df[0]
+    dfSalida['cantidad_de_palabras_clave'] = df[1]
+    dfSalida['porcentaje_de_palabras_clave'] = df[2].str.replace(',', '.')
+    dfSalida.dropna(inplace=True)
+    dfSalida.reset_index(drop=True,inplace=True)
+    dfSalida['cluster'] = dfSalida['cluster'].str.strip().astype(int)
+    dfSalida['cantidad_de_palabras_clave'] = dfSalida['cantidad_de_palabras_clave'].str.strip().astype(int)
+    dfSalida['porcentaje_de_palabras_clave'] = dfSalida['porcentaje_de_palabras_clave'].str.strip().astype(float)
     l = []
     [l.append(i) for i in df[3]]
     pal_clave = ' '.join(l).replace('control multi', 'control.multi')
     l2 = []
     [l2.append(i.strip()) for i in pal_clave[:-1].split('.')]
-    df2['principales_palabras_clave'] = pd.concat([pd.Series(i) for i in l2]).reset_index(drop=True)
-    df2['principales_palabras_clave'] = df2['principales_palabras_clave'].str.replace(' ,', ',').replace(',',', ').str.replace('   ',' ').str.replace('  ',' ').str.strip('\n').str.replace('  ', ' ')
+    dfSalida['principales_palabras_clave'] = pd.concat([pd.Series(i) for i in l2]).reset_index(drop=True)
+    dfSalida['principales_palabras_clave'] = dfSalida['principales_palabras_clave'].str.replace(' ,', ',').replace(',',', ').str.replace('   ',' ').str.replace('  ',' ').str.strip('\n').str.replace('  ', ' ')
 
 
-    return df
+    return dfSalida
